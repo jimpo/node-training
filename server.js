0963b7407e6b3a06661a20c89da17434143cee1b
Jim Posen <jim.posen@gmail.com>
2012-06-20 22:25:22 -0400

    Added Bootstrap.

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.configure(function () {
     app.set('view options', {
         layout: false,
     });
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.get('/', function (req, res, next) {
