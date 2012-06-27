43a66a2b55aa717a9c0bda23e9ed6d5976662c59
Jim Posen <jim.posen@gmail.com>
2012-06-26 20:28:45 -0400

    Added test that database does not get created if it already exists.

--- test/db.js
+++ test/db.js
@@ -23,5 +23,16 @@ describe('db', function () {
                 done(err);
             });
         });
+
+        it('should not create if it already exists', function (done) {
+            var mockDb = {
+                exists: sinon.stub().yields(null, true),
+                create: sinon.stub().yields(),
+            };
+            db.init(mockDb, function (err) {
+                mockDb.create.should.not.have.been.called;
+                done(err);
+            });
+        });
     });
 });
