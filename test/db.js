52c4f925071bef2bb406237f5c061dfa01cc086f
Jim Posen <jim.posen@gmail.com>
2012-06-26 20:57:31 -0400

    Wrote test for Backbone.sync.

--- test/db.js
+++ test/db.js
@@ -1,3 +1,4 @@
+var Backbone = require('backbone');
 var db = require('../lib/db');
 
 
@@ -32,6 +33,33 @@ describe('db', function () {
             db.init(mockDb, function (err) {
                 mockDb.create.should.not.have.been.called;
                 done(err);
+            });
+        });
+    });
+});
+
+describe('Backbone', function () {
+    before(function (done) {
+        db.init(null, done);
+    });
+
+    describe('#sync()', function () {
+        it("should fetch document on 'read' method", function (done) {
+            var model = Backbone.Model.extend({
+                _id: 'pikachu',
+            });
+            var pikachu = {
+                id: 'pikachu',
+                type: 'electric',
+                species: 'mouse',
+            };
+            sinon.stub(db, 'get').withArgs('pikachu').yields(pikachu);
+            Backbone.sync('read', model, {
+                error: done,
+                success: function (res) {
+                    res.should.be.equal(pikachu);
+                    done();
+                },
             });
         });
     });
