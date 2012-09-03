4d3d8e67772dd3ca6146f642936eb4c97c1ee8f1
Jim Posen <jim.posen@gmail.com>
2012-09-02 22:54:37 -0400

    Wrote test verifying database is created if it doesn't exist.

--- test/db.js
+++ test/db.js
@@ -3,6 +3,7 @@
 var nano = require('nano');
 var sinon = require('sinon');
 
+var config = require('../config');
 var db = require('../lib/db');
 
 
@@ -23,24 +24,24 @@ describe('db', function () {
                 done();
             });
         });
+
+        it('should create database if it does not exist', function (done) {
+            sinon.stub(nano.db, 'get').yields(new Error('Database does not exist.'));
+            var mock = sinon.mock(nano.db);
+            mock.expects('create').withArgs(config.dbName).yields();
+            db.init(nano, function (err) {
+                expect(err).not.to.exist;
+                mock.verify();
+                done();
+            });
+        });
     });
 });
 
 
 /*
 describe('db', function () {
-    describe('#init()', function () {
-        it('should check that database exists', function (done) {
-            var mockDb = {
-                exists: sinon.stub().yields(null, true)
-            };
-            db.init(mockDb, function (err) {
-                mockDb.exists.should.have.been.called;
-                done(err);
-            });
-        });
 
-        it('should create database if it does not exist', function (done) {
             var mockDb = {
                 exists: sinon.stub().yields(null, false),
                 create: sinon.stub().yields(),
