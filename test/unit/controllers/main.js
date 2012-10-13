226fa5a50b2f489a2930e729029460fce5b83285
Jim Posen <jim.posen@gmail.com>
2012-10-13 13:18:39 -0400

    Fixed all unit tests.

--- test/unit/controllers/main.js
+++ test/unit/controllers/main.js
@@ -15,33 +15,34 @@ describe('main', function () {
                 _csrf: 'csrf token',
             }
         };
-        res = {};
+        res = {render: sinon.spy()};
         next = sinon.spy();
     });
 
     describe('#home()', function () {
         it('should render home view', function () {
-            res.render = sinon.spy();
             main.home(req, res, next);
             res.render.should.have.been.calledWith('home');
         });
 
         it('should pass signed in user to view', function () {
-            var user = new User('pokefan');
-            sinon.stub(user, 'fetch').yields();
-            res.render = sinon.spy();
-            req.session = {user: user};
+            var user = new User({username: 'pokefan'});
+            sinon.stub(User, 'findOne')
+                .withArgs({username: 'pokefan'})
+                .yields(null, user);
+            req.session = {user: 'pokefan'};
             main.home(req, res, next);
             res.render.should.have.been.calledWith('home');
-            user.fetch.should.have.been.called;
+            User.findOne.should.have.been.called;
+            User.findOne.restore();
         });
     });
 
     describe('#login()', function () {
         it('should render login page', function () {
-            res.render = sinon.spy();
             main.login(req, res, next);
             res.render.should.have.been.calledWith('login', {
+                username: undefined,
                 errors: undefined,
                 token: 'csrf token',
             });
@@ -50,15 +51,15 @@ describe('main', function () {
 
     describe('#loginData()', function () {
         var successfulLogin = function (req, res, next, callback) {
-            sinon.stub(User.prototype, 'fetch').yields(null);
-            sinon.stub(User.prototype, 'matchesPassword').yields(null, true);
+            var user = new User();
+            sinon.stub(User, 'findOne').yields(null, user);
+            sinon.stub(user, 'matchesPassword').yields(null, true);
             req.body = {
                 user: 'pokefan',
                 passwd: 'pikapass',
             };
             res.redirect = function (url) {
-                User.prototype.fetch.restore();
-                User.prototype.matchesPassword.restore();
+                User.findOne.restore();
                 callback(url);
             };
             main.loginData(req, res, next);
@@ -66,70 +67,68 @@ describe('main', function () {
 
         it('should fail if username is not provided', function () {
             req.body = {passwd: 'pikapass'};
-            res.render = sinon.spy();
             main.loginData(req, res, next);
             res.render.should.have.been.calledWith('login');
-            res.render.args[0][1].errors.should.contain(
-                'Please enter username');
+            res.render.firstCall.args[1].errors
+                .should.have.property('username');
         });
 
         it('should fail if password is not provided', function () {
             req.body = {user: 'pokefan'};
-            res.render = sinon.spy();
             main.loginData(req, res, next);
             res.render.should.have.been.calledWith('login');
-            res.render.args[0][1].errors.should.contain(
-                'Please enter password');
+            res.render.firstCall.args[1].errors
+                .should.have.property('passwd');
         });
 
         it('should render with user on failure', function () {
             req.body = {user: 'pokefan'};
-            res.render = sinon.spy();
             main.loginData(req, res, next);
             res.render.should.have.been.calledWith('login');
-            res.render.args[0][1].errors.should.exist;
-            res.render.args[0][1].user.should.equal('pokefan');
+            res.render.firstCall.args[1].errors.should.exist;
+            res.render.firstCall.args[1].username.should.equal('pokefan');
         });
 
-        it('should fetch given user', function () {
-            sinon.stub(User.prototype, 'fetch');
+        it('should find given user', function () {
+            sinon.stub(User, 'findOne').yields();
             req.body = {
                 user: 'pokefan',
                 passwd: 'pikapass',
             };
             main.loginData(req, res, next);
-            User.prototype.fetch.should.have.been.called;
-            User.prototype.fetch.restore();
+            User.findOne.should.have.been.called;
+            User.findOne.restore();
         });
 
         it('should fail if user doesn\'t exist', function () {
-            sinon.stub(User.prototype, 'fetch').yields(errs.create({
-                status_code: 404,
-            }));
+            sinon.stub(User, 'findOne')
+                .withArgs({username: 'pokefan'})
+                .yields();
             req.body = {
                 user: 'pokefan',
                 passwd: 'pikapass',
             };
-            res.render = sinon.spy();
             main.loginData(req, res, next);
             res.render.should.have.been.calledWith('login');
-            res.render.args[0][1].errors.should.contain(
-                'User "pokefan" does not exist');
-            User.prototype.fetch.restore();
+            res.render.firstCall.args[1].errors.username.should.deep.equal({
+                message: 'User "pokefan" does not exist',
+            });
+            User.findOne.restore();
         });
 
         it('should fail if password is incorrect', function (done) {
-            sinon.stub(User.prototype, 'fetch').yields(null);
-            sinon.stub(User.prototype, 'matchesPassword').yields(null, false);
+            var user = new User();
+            sinon.stub(User, 'findOne').yields(null, user);
+            sinon.stub(user, 'matchesPassword').yields(null, false);
             req.body = {
                 user: 'pokefan',
                 passwd: 'pikapass',
             };
             res.render = function (view, locals) {
                 view.should.equal('login');
-                locals.errors.should.contain('Password did not match');
-                User.prototype.fetch.restore();
-                User.prototype.matchesPassword.restore();
+                locals.errors.passwd.message.should.equal(
+                    'Password did not match');
+                User.findOne.restore();
                 done();
             };
             main.loginData(req, res, next);
@@ -140,7 +139,7 @@ describe('main', function () {
             req.query = {};
             successfulLogin(req, res, next, function () {
                 req.session.should.have.property('user');
-                req.session.user.id().should.equal('pokefan');
+                req.session.user.should.equal('pokefan');
                 done();
             });
         });
